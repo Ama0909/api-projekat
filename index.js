972561d946f61c4b8ae3549f74cf27501eb4c28f
Ama0909 <muratovic.amir2000@gmail.com>
2023-05-16 01:36:10 +0200

    Zadatak

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 const express = require("express");
 const app = express();
 const request = require("request");
+const auth = require('basic-auth');
+
 
 const PORT = process.env.PORT || 3001;
 const apiKey = '442d74bed1674ff492542e63cd69c772';
+
+const validCredentials = {
+
+    username: "ama09",
+    password: "1234"
+
+};
+
+function checkCredentials(username, password) {
+
+    // Implement the logic to check the username and password
+
+    return username === process.env.USERNAME && password === process.env.PASSWORD;
+
+}
+
+const authMiddleware = (req, res, next) => {
+
+    const credentials = auth(req);
+
+    if (!credentials || !checkCredentials(credentials.name, credentials.pass)) {
+
+        res.status(401).set('WWW-Authenticate', 'Basic realm="Authentication Required"').send('Unauthorized');
+
+    } else {
+
+        next();
+
+    }
+
+}
+
 
 
 app.listen(PORT, function () {
     console.log("Node Js Server is Running");
 })
 
-app.get('/weather/current/:location', function (req, res) {
+app.get('/weather/current/:location', authMiddleware, function (req, res) {
 
 
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${req.params.location}&units=metric&appid=${apiKey}`;
@@ -25,7 +59,7 @@ app.get('/weather/current/:location', function (req, res) {
     })
 })
 
-app.get('/weather/forecast/:location', function (req, res) {
+app.get('/weather/forecast/:location', authMiddleware, function (req, res) {
 
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${req.params.location}&units=metric&appid=${apiKey}`;
 
@@ -43,7 +77,7 @@ app.get('/weather/forecast/:location', function (req, res) {
 
 })
 
-app.get('/weather/history/:location', function (req, res) {
+app.get('/weather/history/:location', authMiddleware, function (req, res) {
 
     const url = `https://api.openweathermap.org/data/2.5/history/city?q=${req.params.location}&type=hour&units=metric&appid=${apiKey}`;
 
@@ -56,3 +90,4 @@ app.get('/weather/history/:location', function (req, res) {
         }
     })
 })
+
